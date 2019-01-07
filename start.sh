0bf197b601c99576c950d94e27372236c66a0ebf
Dondrey Taylor <dondrey.taylor@gmail.com>
2019-01-06 20:41:35 -0500

    Updated magic peer in other files

--- start.sh
+++ start.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-# --- bitcoin mainnet: f9beb4d9 (db = 0) ---
-python -u crawl.py conf/crawl.f9beb4d9.conf master > log/crawl.f9beb4d9.master.out 2>&1 &
-python -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.1.out 2>&1 &
-python -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.2.out 2>&1 &
-python -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.3.out 2>&1 &
-python -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.4.out 2>&1 &
-python -u crawl.py conf/crawl.f9beb4d9.conf slave > log/crawl.f9beb4d9.slave.5.out 2>&1 &
+# --- bitcoin mainnet: e3486a45 (db = 0) ---
+python -u crawl.py conf/crawl.e3486a45.conf master > log/crawl.e3486a45.master.out 2>&1 &
+python -u crawl.py conf/crawl.e3486a45.conf slave > log/crawl.e3486a45.slave.1.out 2>&1 &
+python -u crawl.py conf/crawl.e3486a45.conf slave > log/crawl.e3486a45.slave.2.out 2>&1 &
+python -u crawl.py conf/crawl.e3486a45.conf slave > log/crawl.e3486a45.slave.3.out 2>&1 &
+python -u crawl.py conf/crawl.e3486a45.conf slave > log/crawl.e3486a45.slave.4.out 2>&1 &
+python -u crawl.py conf/crawl.e3486a45.conf slave > log/crawl.e3486a45.slave.5.out 2>&1 &
 
-python -u ping.py conf/ping.f9beb4d9.conf master > log/ping.f9beb4d9.master.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.1.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.2.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.3.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.4.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.5.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.6.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.7.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.8.out 2>&1 &
-python -u ping.py conf/ping.f9beb4d9.conf slave > log/ping.f9beb4d9.slave.9.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf master > log/ping.e3486a45.master.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.1.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.2.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.3.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.4.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.5.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.6.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.7.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.8.out 2>&1 &
+python -u ping.py conf/ping.e3486a45.conf slave > log/ping.e3486a45.slave.9.out 2>&1 &
 
-python -u resolve.py conf/resolve.f9beb4d9.conf > log/resolve.f9beb4d9.out 2>&1 &
+python -u resolve.py conf/resolve.e3486a45.conf > log/resolve.e3486a45.out 2>&1 &
 
-python -u export.py conf/export.f9beb4d9.conf > log/export.f9beb4d9.out 2>&1 &
+python -u export.py conf/export.e3486a45.conf > log/export.e3486a45.out 2>&1 &
 
-python -u seeder.py conf/seeder.f9beb4d9.conf > log/seeder.f9beb4d9.out 2>&1 &
+python -u seeder.py conf/seeder.e3486a45.conf > log/seeder.e3486a45.out 2>&1 &
 
-python -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.1.out 2>&1 &
-python -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.2.out 2>&1 &
-python -u pcap.py conf/pcap.f9beb4d9.conf > log/pcap.f9beb4d9.3.out 2>&1 &
+python -u pcap.py conf/pcap.e3486a45.conf > log/pcap.e3486a45.1.out 2>&1 &
+python -u pcap.py conf/pcap.e3486a45.conf > log/pcap.e3486a45.2.out 2>&1 &
+python -u pcap.py conf/pcap.e3486a45.conf > log/pcap.e3486a45.3.out 2>&1 &
 
 # --- bitcoin testnet3: 0b110907 (db = 1) ---
 python -u crawl.py conf/crawl.0b110907.conf master > log/crawl.0b110907.master.out 2>&1 &
